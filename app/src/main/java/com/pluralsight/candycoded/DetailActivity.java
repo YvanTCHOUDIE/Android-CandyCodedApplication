e610546688fcca7500b33eab53372b1f49dc6bdc
Yvan TCHOUDIE <yvan.tchoudie@gmail.com>
2020-10-07 22:50:30 +0100

    Completed module

--- app/src/main/java/com/pluralsight/candycoded/DetailActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/DetailActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -71,4 +72,26 @@ public class DetailActivity extends AppCompatActivity {
     // ***
     // TODO - Task 4 - Share the Current Candy with an Intent
     // ***
+
+    /* DONE - Share the Current Candy with an Intent
+        Here we start the InfoActivity upon an item selection from the menu.
+        @Author Yvan TCHOUDIE DJOMESSI
+    */
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        createShareIntent();
+        return super.onOptionsItemSelected(item);
+
+    }
+
+    private void createShareIntent() {
+
+        Intent shareIntent = new Intent(Intent.ACTION_SEND);
+        shareIntent.setType("text/plain");
+        shareIntent.putExtra(Intent.EXTRA_TEXT, SHARE_DESCRIPTION + mCandyImageUrl + HASHTAG_CANDYCODED);
+        startActivity(shareIntent);
+
+    }
 }
